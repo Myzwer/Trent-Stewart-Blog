2f0313f19bab7de577e3bb448328da4c490b30c5
Josh Forrester <twicearoundmars@yahoo.com>
2019-10-10 12:27:50 -0400

    added 404 page

---
+++ app/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ */
+
+get_header();
+?>
+
+<div class = "featured-pages">
+    <div class="grid-container fullWidth billboard-404">
+        <div class="grid-x grid-padding-x">
+            <div class="large-12 cell billboard-main">
+                <h1 class = "center">God Moves in mysterious ways.</h1>
+                <h2 class = "center">But this website shouldn't. You've found a broken link.</h2>
+                <div class = "center">
+				<h3>Let's get you where you were going.</h3>
+				<div class = "center">
+                        <a class="ff dark1" href="http://trentstewart.org/">Homepage</a> 
+                         <a class="ff dark1" href="http://trentstewart.org/consult">Consulting</a> 
+                          <a class="ff dark1" href="http://trentstewart.org/blog">Blog</a> 
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
